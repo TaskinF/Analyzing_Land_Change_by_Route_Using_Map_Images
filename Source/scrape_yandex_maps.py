c903f554d68343a66817d4db8a5c140863bdcd96
furka <furkantaskin@std.iyte.edu.tr>
2023-12-11 14:19:51 +0300

    Create route_steps list from Latitude and Longitude columns in the Excel file

--- Source/scrape_yandex_maps.py
+++ Source/scrape_yandex_maps.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import pandas as pd
 import time
 
 # Path to Chromedriver
@@ -7,7 +8,6 @@ driver_path = r"C:\Users\furka\Downloads\chromedriver-win64\chromedriver.exe"
 
 # Configure Chrome options
 chrome_options = Options()
-# Add any additional options if needed
 
 # Specify the Chromedriver executable path directly
 chrome_options.binary_location = driver_path
@@ -15,21 +15,21 @@ chrome_options.binary_location = driver_path
 # Start Chromedriver with specified options
 driver = webdriver.Chrome(options=chrome_options)
 
-# Define your route steps here
-route_steps = [
-    (38.4251379, -80.1856578),  # Appalachian Mountains koordinatları
-    (36.950932321950035, -81.08864802133368),
-    (36.945826169037986, -80.94991110679278),
-    # ... Diğer rota adımları ...
-    (39.9512496, -76.7336521),  # York County koordinatları
-]
+# Setting the windows size
+driver.set_window_size(1920, 1080)
+
+# Read coordinates from Excel file
+excel_file = r"C:\Users\furka\PycharmProjects\Automatic_Cross_Sectional_Surface_Analysis_from_Aerial_Photographs\Automatic-Cross-Sectional-Surface-Analysis-from-Aerial-Photographs\Source\route_coordinates.xlsx"
+df = pd.read_excel(excel_file)
+
+# Create route_steps list from Latitude and Longitude columns in the Excel file
+route_steps = [(lat, lon) for lat, lon in zip(df['Latitude'], df['Longitude'])]
 
 # Visit each coordinate in the route steps and take screenshots
 for index, step in enumerate(route_steps, start=1):
-    url = f"https://yandex.com.tr/harita/?l=sat&ll={step[1]},{step[0]}&z=17"
+    url = f"https://yandex.com.tr/harita/?l=sat&ll={step[1]},{step[0]}&z=18.5"
     driver.get(url)
-    time.sleep(5)  # Haritanın yüklenmesini beklemek için bir süre bekleme
-
+    time.sleep(3)
     # Ekran görüntüsü al
     driver.save_screenshot(f'yandex_harita_goruntusu_{index}.png')
 

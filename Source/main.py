bd0f242107539e5f90f9d8b3f7b45f419359d552
furka <furkantaskin@std.iyte.edu.tr>
2023-12-29 13:54:35 +0300

    Cropping the images and finding the heading

--- Source/main.py
+++ Source/main.py
@@ -1,40 +1,65 @@
-import cv2
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.semi_supervised import SelfTrainingClassifier
-from sklearn.svm import SVC
+import math
+from PIL import Image
+import os
+# Function to calculate the bearing between two points given their latitude and longitude
+def calculate_bearing(lat1, lon1, lat2, lon2):
+    # Convert angles to radians
+    lat1 = math.radians(lat1)
+    lon1 = math.radians(lon1)
+    lat2 = math.radians(lat2)
+    lon2 = math.radians(lon2)
 
-# Sample data
-# X: Features, y: Labels
-X = []  # Data representing features
-y = []  # Data representing labels
+    # Calculate the difference in longitudes
+    delta_lon = lon2 - lon1
 
-# Extract Haralick features from images
-for index, step in enumerate(route_steps, start=1):
-    image = cv2.imread(f'yandex_harita_goruntusu_{index}.png')  # Load image
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # Convert image to grayscale
+    # Calculate the bearing
+    y = math.sin(delta_lon) * math.cos(lat2)
+    x = math.cos(lat1) * math.sin(lat2) - math.sin(lat1) * math.cos(lat2) * math.cos(delta_lon)
+    bearing = math.atan2(y, x)
 
-    # Performing texture analysis to obtain Haralick features
-    textures = cv2.imgproc.TextureLBP_create()
-    texture = textures.computeLBP(gray_image)
+    # Convert the angle from radians to degrees
+    bearing = math.degrees(bearing)
 
-    # Extract Haralick features
-    haralick_features = np.ravel(texture)
+    # Ensure the angle is within the range of 0-360 degrees
+    bearing = (bearing + 360) % 360
 
-    # Add extracted features to X list
-    X.append(haralick_features)
+    return bearing
 
-    # For example, add labels corresponding to each image to the y list
-    # For instance, assigning '1' and '0' as labels
-    y.append(1 if condition else 0)  # Replace 'condition' with your condition to assign labels
+# Example: Calculate the bearing between two coordinates
+lat1 = 38.44933  # Harrisonburg, Virginia
+lon1 = -78.8689
+lat2 = 35.75  # Blue Ridge Mountain, USA
+lon2 = -82.25
 
-# Train-test split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+result_bearing = calculate_bearing(lat1, lon1, lat2, lon2)
+print(f"The bearing between the two points is: {result_bearing} degrees")
 
-# Semi-Supervised SVM (Semi-SVM) Model
-base_classifier = SVC(probability=True)  # Using SVC as the base classifier
-model = SelfTrainingClassifier(base_classifier, criterion="k_best", k_best=50)  # Creating a Semi-Supervised SVM
-model.fit(X_train, y_train)  # Training the model with the training data
+# Get all image files in the folder
+image_files = [file for file in os.listdir(".") if file.endswith(".png")]  # or update according to the file extension
 
-# Making predictions on the test data
-predictions = model.predict(X_test)  # Making predictions on the test data
+# Cropping from the left side (for example, 400 pixels width)
+crop_width = 400
+
+for filename in image_files:
+    # Open each image
+    image = Image.open(filename)
+
+    # Crop from the specified width on the left side
+    cropped_image = image.crop((crop_width, 0, image.width, image.height))
+
+    # Save the updated image
+    updated_filename = "updated_" + filename  # You can create a new file name
+    cropped_image.save(updated_filename)
+
+    # Clear memory
+    image.close()
+"""
+# İki görüntüyü aynı boyutlara getirme (örneğin, 800x600)
+new_width = 1500
+new_height = 897
+image_1 = image_1.resize((new_width, new_height), Image.BILINEAR)
+image_2 = image_2.resize((new_width, new_height), Image.BILINEAR)
+
+image_1.save("resized_image_1.png")
+image_2.save("resized_image_2.png")
+"""
